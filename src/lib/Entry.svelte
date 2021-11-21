366f7032cbeebfda8fe20082378e3e09bf6ca3e2
zhetiew <zhetiew@gmail.com>
2021-11-21 06:50:03 +0000

    added entry

---
+++ src/lib/Entry.svelte
@@ -0,0 +1,7 @@
+<button type="button" class="list-group-item list-group-item-action">
+	<div class="d-flex w-100 justify-content-between lh-1">
+    	<h6 class="my-0">1 Nov 2021</h6>
+    	<span class="text-success">Best</span>
+	</div>
+	<small class="text-muted">Comments on medication, activities, etc.</small>
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,3 +3,16 @@ import Greeting from "$lib/Greeting.svelte";
 
 </script>
 <Greeting />
+<!-- Entries -->
+<section class="container px-4 py-3">
+    <div class="d-flex justify-content-between">
+   	 <div class="p-2">Mood Log</div>
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+    </div>
+
+    <div class="list-group mb-3">
+   	 <!-- Individual Entries -->
+    	<Entry />
+   	 
+    </div>
+</section>
