ce8e54e9f6a60d5c3e80ce4d638c9e6040dedadb
zhetiew <zhetiew@gmail.com>
2021-12-05 07:30:49 +0000

    done till step 27*

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -1,7 +1,24 @@
+<script>
+	export let date; // 1-1-2021
+	export let mood; // Best
+	export let comment; // Comments on medication, activities, etc.
+</script>
+
 <button type="button" class="list-group-item list-group-item-action">
-	<div class="d-flex w-100 justify-content-between lh-1">
-    	<h6 class="my-0">1 Nov 2021</h6>
-    	<span class="text-success">Best</span>
-	</div>
-	<small class="text-muted">Comments on medication, activities, etc.</small>
+    <div class="d-flex w-100 justify-content-between lh-1">
+   	 <h6 class="my-0">{date}</h6>
+
+   	 {#if mood === 'Worst'}
+   		 <span class="text-danger">{mood}</span>
+   	 {:else if mood === 'Bad'}
+   		 <span class="text-warning">{mood}</span>
+   	 {:else if mood === 'Okay'}
+   		 <span class="text-primary">{mood}</span>
+   	 {:else if mood === 'Good'}
+   		 <span class="text-info">{mood}</span>
+   	 {:else if mood === 'Best'}
+   		 <span class="text-success">{mood}</span>
+   	 {/if}
+    </div>
+    <small class="text-muted">{comment}</small>
 </button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,46 @@
 <script>
-import Entry from "$lib/Entry.svelte";
-import EntryModal from "$lib/EntryModal.svelte";
-import Greeting from "$lib/Greeting.svelte";
-import supabase from '$lib/db';
-import { goto } from '$app/navigation';
+	import Entry from '$lib/Entry.svelte';
+	import EntryModal from '$lib/EntryModal.svelte';
+	import Greeting from '$lib/Greeting.svelte';
+	import supabase from '$lib/db';
+	import { goto } from '$app/navigation';
 
-async function signOut() {
-const { error } = await supabase.auth.signOut();
+	async function signOut() {
+		const { error } = await supabase.auth.signOut();
 
-if (error) alert(error.message); // alert if error
-goto('/login');
-}
+		if (error) alert(error.message); // alert if error
+		goto('/login');
+	}
+	// Select entries
+	async function getEntries() {
+		const { data, error } = await supabase.from('moodEntries').select();
+		if (error) alert(error.message);
+
+		return data;
+	}
 </script>
+
 <Greeting />
 <!-- Entries -->
 <section class="container px-4 py-3">
-    <div class="d-flex justify-content-between">
-   	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#newEntry"/>
-    </div>
+	<div class="d-flex justify-content-between">
+		<div class="p-2">Mood Log</div>
+		<input
+			class="btn btn-light mb-2"
+			type="button"
+			value="+ New Entry"
+			data-bs-toggle="modal"
+			data-bs-target="#newEntry"
+		/>
+	</div>
 
-    <div class="list-group mb-3">
-   	 <!-- Individual Entries -->
-    	<Entry />
-   	 
-    </div>
+	<div class="list-group mb-3">
+		<!-- Individual Entries -->
+		<Entry />
+	</div>
 </section>
 <EntryModal />
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
-    <button class="btn btn-secondary" on:click={signOut}>Logout</button>
+	<button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
