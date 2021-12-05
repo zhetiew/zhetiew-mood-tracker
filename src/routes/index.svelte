fbfdac8ee875b5765d70a32688a735f95ddd3621
zhetiew <zhetiew@gmail.com>
2021-12-05 07:33:21 +0000

    done?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,6 +36,21 @@
 
 	<div class="list-group mb-3">
 		<!-- Individual Entries -->
+		<!-- Individual Entries -->
+		{#await getEntries()}
+			<p>Fetching data...</p>
+		{:then data}
+			{#each data as entry}
+				<Entry
+					date={entry.day + '-' + entry.month + '-' + entry.year}
+					mood={entry.mood}
+					comment={entry.comment}
+				/>
+			{/each}
+		{:catch error}
+			<p>Something went wrong while fetching the data:</p>
+			<pre>{error}</pre>
+		{/await}
 		<Entry />
 	</div>
 </section>
